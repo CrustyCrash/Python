4d9622953e32494b38e60e2e62d43ff6ead1883a
CrustyCrash <167889402+CrustyCrash@users.noreply.github.com>
2024-05-01 08:40:13 +0530

    Delete cmToFeet.py

--- cmToFeet.py
+++
@@ -1,8 +0,0 @@
-# Get the height from the user in cms and display the user height back to console
-# in foot/feet and inches eg : 155 in cms displays 5.09 in feet and inches
-
-print("Enter height in cms")
-cm_height = int(input())
-
-print("Converted height: ", cm_height*0.0328, "Foot")
-
